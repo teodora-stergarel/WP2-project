9c0f84e313d7888d8b2144993053166421781396
Stergarel Teodora <teodora@Stergarels-MacBook-Air.local>
2026-03-11 01:39:08 +0200

    set up initial Spring Boot project

---
+++ src/test/java/com/wp/skillswap/SkillswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wp.skillswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
