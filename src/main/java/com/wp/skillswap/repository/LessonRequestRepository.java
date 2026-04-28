d79aaa4e8aec797c7b893d0c334edb1efaccdac9
unknown <rus.alexa.2004@gmail.com>
2026-04-28 17:08:20 +0300

    Add missing lesson request repository methods

--- src/main/java/com/wp/skillswap/repository/LessonRequestRepository.java
+++ src/main/java/com/wp/skillswap/repository/LessonRequestRepository.java
@@ -2,9 +2,18 @@ package com.wp.skillswap.repository;
 
 import com.wp.skillswap.model.LessonRequest;
 import com.wp.skillswap.model.Offer;
+import com.wp.skillswap.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface LessonRequestRepository extends JpaRepository<LessonRequest, Long> {
 
     void deleteByOffer(Offer offer);
+
+    void deleteByRequester(User requester);
+
+    List<LessonRequest> findByRequester(User requester);
+
+    List<LessonRequest> findByOfferOwner(User owner);
 }
