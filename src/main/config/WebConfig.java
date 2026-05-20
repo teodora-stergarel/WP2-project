96223c2ca23211c7e98dbd1d8471f81982f0fc0e
“teodora-stergarel” <teodora.stergarel@stud.fils.upb.ro>
2026-05-21 02:50:05 +0300

    Added attachment feature

--- src/main/config/WebConfig.java
+++ src/main/config/WebConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.File;
+import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String uploadPath = System.getProperty("user.dir") + File.separator + "uploads" + File.separator;
+        String uploadPath = Paths.get("uploads").toAbsolutePath().toUri().toString();
 
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:///" + uploadPath.replace("\\", "/"));
+                .addResourceLocations(uploadPath);
     }
 }
